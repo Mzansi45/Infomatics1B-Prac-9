a0d05d5855e089738d2cd3b53fffa2c0001b784f
Mzansi45 <92696804+Mzansi45@users.noreply.github.com>
2022-09-08 14:14:09 +0200

    first commit

---
+++ S219020988_P2020A-09/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmF2H
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInitialize = New System.Windows.Forms.Button()
+        Me.btnCapture = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnMax = New System.Windows.Forms.Button()
+        Me.grid1 = New UJGrid.UJGrid()
+        Me.SuspendLayout()
+        '
+        'btnInitialize
+        '
+        Me.btnInitialize.Location = New System.Drawing.Point(12, 12)
+        Me.btnInitialize.Name = "btnInitialize"
+        Me.btnInitialize.Size = New System.Drawing.Size(120, 46)
+        Me.btnInitialize.TabIndex = 0
+        Me.btnInitialize.Text = "INITIALIZE AND LABEL GRID"
+        Me.btnInitialize.UseVisualStyleBackColor = True
+        '
+        'btnCapture
+        '
+        Me.btnCapture.Location = New System.Drawing.Point(169, 12)
+        Me.btnCapture.Name = "btnCapture"
+        Me.btnCapture.Size = New System.Drawing.Size(122, 46)
+        Me.btnCapture.TabIndex = 1
+        Me.btnCapture.Text = "CAPTURE INFORMATION"
+        Me.btnCapture.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(326, 12)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(121, 46)
+        Me.btnCalculate.TabIndex = 2
+        Me.btnCalculate.Text = "CALCULATE"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnMax
+        '
+        Me.btnMax.Location = New System.Drawing.Point(494, 12)
+        Me.btnMax.Name = "btnMax"
+        Me.btnMax.Size = New System.Drawing.Size(119, 46)
+        Me.btnMax.TabIndex = 3
+        Me.btnMax.Text = "BUSIEST"
+        Me.btnMax.UseVisualStyleBackColor = True
+        '
+        'grid1
+        '
+        Me.grid1.FixedCols = 1
+        Me.grid1.FixedRows = 1
+        Me.grid1.Location = New System.Drawing.Point(12, 65)
+        Me.grid1.Name = "grid1"
+        Me.grid1.Scrollbars = System.Windows.Forms.ScrollBars.Both
+        Me.grid1.Size = New System.Drawing.Size(601, 290)
+        Me.grid1.TabIndex = 4
+        '
+        'FrmF2H
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(624, 363)
+        Me.Controls.Add(Me.grid1)
+        Me.Controls.Add(Me.btnMax)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnCapture)
+        Me.Controls.Add(Me.btnInitialize)
+        Me.Name = "FrmF2H"
+        Me.Text = "FORM 2 HEALTH"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnInitialize As Button
+    Friend WithEvents btnCapture As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnMax As Button
+    Friend WithEvents grid1 As UJGrid.UJGrid
+End Class
---
+++ S219020988_P2020A-09/Form1.vb
@@ -0,0 +1,149 @@
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+'************************************************
+'Surname Initials: Gulube T
+'Student Number: 219020988
+'Practical: P2020A-09
+'Class Name:
+'************************************************
+
+Public Class FrmF2H
+    'these are my variable declerations
+    Private regions As Integer
+    Private months As Integer
+    Private provinces As Integer
+    Private province() As ProvinceRec
+
+    Private Structure RegionRec  'record structure for each region
+        Public LeaderName As String
+        Public nClinics As Integer
+        Public Month() As Integer
+        Public totalVisitors As Integer
+        Public ClinicRatio As Double
+    End Structure
+    Private Structure ProvinceRec   'rechord structure for each province
+        Public name As String
+        Public region() As RegionRec
+        Public max As Integer
+    End Structure
+
+    Private Sub inputPRM()
+        'these are the primary inputs  they are compalsory for app to work
+        provinces = CInt(InputBox("Number of Provinces", "Mem"))
+        regions = CInt(InputBox("Number of regions", "Mem"))
+        months = CInt(InputBox("Number of months", "Mem"))
+    End Sub
+    'the sub below helps me label the grid
+    Private Sub LG(ByVal c As Integer, ByVal r As Integer, ByVal t As String)
+        grid1.Col = c
+        grid1.Row = r
+        grid1.Text = t
+    End Sub
+
+    Private Sub ResizeAll() ' this sub is not only for resizing but for labelling the grid as well
+
+        grid1.Cols = regions + 2
+        grid1.Rows = provinces + 1
+
+        ReDim province(provinces)
+        For p As Integer = 1 To provinces
+            ReDim province(p).region(regions)
+            For r As Integer = 1 To regions
+                ReDim province(p).region(r).Month(months)
+            Next
+        Next
+
+        LG(0, 0, "Province Name")
+        LG(regions + 1, 0, "Busiest region")
+        For p As Integer = 1 To provinces
+            LG(0, p, "Province " & CStr(p))
+        Next p
+
+        For r As Integer = 1 To regions
+            LG(r, 0, "Reg" & CStr(r) & " clinic Ratio")
+        Next
+    End Sub
+
+    Private Sub CalcTotal(ByVal number As Integer, ByRef size As Integer) ' with this sub i can calculate any total
+        size += number
+    End Sub
+
+    Private Sub UserInput() 'all data will be captured here in this sub
+        For p As Integer = 1 To provinces
+            province(p).name = InputBox("Name of province " & CStr(p))
+            For r As Integer = 1 To regions
+                province(p).region(r).LeaderName = InputBox("Please enter the name of the leader of region " & CStr(r) & " in " & province(p).name)
+                province(p).region(r).nClinics = CInt(InputBox("Number of Clinics in region " & CStr(r) & " in " & province(p).name))
+                For m As Integer = 1 To months
+                    province(p).region(r).Month(m) = CInt(InputBox("Please enter the number of Patients in Region " & CStr(r) & " Month " & CStr(m) & province(p).name))
+                Next m
+            Next r
+            LG(0, p, province(p).name)
+        Next p
+    End Sub
+
+    Private Sub Total() 'the total number of patients
+        For p As Integer = 1 To provinces
+            For r As Integer = 1 To regions
+                For m As Integer = 1 To months
+                    CalcTotal(province(p).region(r).Month(m), province(p).region(r).totalVisitors)
+                Next m
+            Next r
+        Next p
+    End Sub
+
+    Private Sub clinicRatio() ''this sub helps me calculate the clinic ratio 
+        For p As Integer = 1 To provinces
+            For r As Integer = 1 To regions
+                Select Case province(p).region(r).nClinics
+                    Case 0 To 12
+                        province(p).region(r).ClinicRatio = province(p).region(r).totalVisitors / months
+                    Case 13 To 25
+                        province(p).region(r).ClinicRatio = province(p).region(r).totalVisitors / 10
+                    Case > 25
+                        province(p).region(r).ClinicRatio = province(p).region(r).totalVisitors / 20
+                End Select
+                LG(r, p, CStr(province(p).region(r).ClinicRatio)) 'grid label
+            Next r
+        Next p
+    End Sub
+
+    Private Sub BestRegion() 'under this sub i calculate the busiest region using a select case
+        For p As Integer = 1 To provinces
+            Dim best As Integer
+            best = CInt(province(p).region(1).ClinicRatio)
+            province(p).max = 1
+            For r As Integer = 1 To regions
+                Select Case best
+                    Case < CInt(province(p).region(r).ClinicRatio)
+                        best = CInt(province(p).region(r).ClinicRatio)
+                        province(p).max = r
+                End Select
+            Next r
+            LG(regions + 1, p, province(p).region(province(p).max).LeaderName) ' grid label
+        Next p
+
+    End Sub
+
+    'the following code has a lot of button clicks. this is where all my subs work
+    Private Sub btnInitialize_Click(sender As Object, e As EventArgs) Handles btnInitialize.Click
+        inputPRM()
+        ResizeAll()
+    End Sub
+
+    Private Sub btnCapture_Click(sender As Object, e As EventArgs) Handles btnCapture.Click
+        UserInput()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Total()
+        clinicRatio()
+    End Sub
+
+    Private Sub btnMax_Click(sender As Object, e As EventArgs) Handles btnMax.Click
+        BestRegion()
+    End Sub
+
+End Class
+
